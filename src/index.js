28f9d1ad44b0e19fc2db7f71baa61605f19db677
duy6861 <duy6861@gmail.com>
2022-04-12 13:11:44 +0700

    second

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const app = express();
+const port = 3000;
+//HTTP Lokger
+app.use(morgan('combined'));
+//Template angine
+app.engine('hbs', engine({ extname: '.hbs', defaultLayout: "main"}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources\\views'));
+console.log(path.join(__dirname, 'resources\\views'));
+
+app.get('/', (req, res) => {
+    
+  res.render('home');
+})
+app.get('/news', (req, res) => {
+    
+  res.render('news');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
